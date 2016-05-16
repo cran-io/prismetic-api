e9ff1a2139d2de71eb41e334d3c6e707d4f13ab3
Gonzalo Pombo <gonpombo@gmail.com>
2016-05-16 12:24:31 -0300

    Device without account.

--- app/controllers/devices.js
+++ app/controllers/devices.js
@@ -2,22 +2,30 @@ var Device   = require('../models/device');
 var Account   = require('../models/account');
 var exports  = module.exports;
 
+// exports.create = (request, response) => {
+//   if(!request.body.accountId) return response.send(400);
+//   Account.findById(request.body.accountId, (error, account) => {
+//     if(error) return res.send(500);
+//     if(!account) return res.send(400, {message: "No se encontro un Account con ese id"});
+//     var device = new Device();
+//     device.model = request.body.model;
+//     device.active = request.body.active;
+//     device.save((error, device) => {
+//       if (error) return response.send(500, error);
+//       account.devices.push(device);
+//       account.save((error, account) => {
+//         if(error) return res.send(500);
+//         response.json(device);
+//       });
+//     });
+//   });
+// };
+
 exports.create = (request, response) => {
-  if(!request.body.accountId) return response.send(400);
-  Account.findById(request.body.accountId, (error, account) => {
-    if(error) return res.send(500);
-    if(!account) return res.send(400, {message: "No se encontro un Account con ese id"});
-    var device = new Device();
-    device.model = request.body.model;
-    device.active = request.body.active;
-    device.save((error, device) => {
-      if (error) return response.send(500, error);
-      account.devices.push(device);
-      account.save((error, account) => {
-        if(error) return res.send(500);
-        response.json(device);
-      });
-    });
+  var device = new Device(request.body);
+  device.save((error, device) => {
+    if (error) return response.send(500, error);
+    response.json(device);
   });
 };
 
