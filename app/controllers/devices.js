a36899f93ae321ffdaaf760bf82b3af8bfb8b888
Gonzalo Pombo <gonpombo@gmail.com>
2016-05-26 11:26:29 -0300

    account with devices.

--- app/controllers/devices.js
+++ app/controllers/devices.js
@@ -1,31 +1,33 @@
 var Device   = require('../models/device');
 var Account   = require('../models/account');
-// exports.create = (request, response) => {
-//   if(!request.body.accountId) return response.send(400);
-//   Account.findById(request.body.accountId, (error, account) => {
-//     if(error) return res.send(500);
-//     if(!account) return res.send(400, {message: "No se encontro un Account con ese id"});
-//     var device = new Device();
-//     device.model = request.body.model;
-//     device.active = request.body.active;
-//     device.save((error, device) => {
-//       if (error) return response.send(500, error);
-//       account.devices.push(device);
-//       account.save((error, account) => {
-//         if(error) return res.send(500);
-//         response.json(device);
-//       });
-//     });
-//   });
-// };
 
-exports.create = (request, response, next) => {
-  var device = new Device(request.body);
-  device.save((error, device) => {
-    if(error) return next(error);
-    response.json(device);
+
+exports.create = (request, response) => {
+  if(!request.body.account) return response.send(400);
+  Account.findById(request.body.account, (error, account) => {
+    if(error) return res.send(500);
+    if(!account) return res.send(400, {message: "No se encontro un Account con ese id"});
+    var device = new Device();
+    device.model = request.body.model;
+    device.active = request.body.active;
+    device.save((error, device) => {
+      if (error) return response.send(500, error);
+      account.devices.push(device);
+      account.save((error, account) => {
+        if(error) return res.send(500);
+        response.json(device);
+      });
+    });
   });
 };
+
+// exports.create = (request, response, next) => {
+//   var device = new Device(request.body);
+//   device.save((error, device) => {
+//     if(error) return next(error);
+//     response.json(device);
+//   });
+// };
 
 exports.index = (request, response, next) => {
   Device.find((error, devices) => {
